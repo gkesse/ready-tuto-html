14846d0bf134edcfe00ede1ded7e20df749e6eb4
Gerard KESSE <tiakagerard@hotmail.com>
2024-06-17 00:05:06 +0200

    tree

--- 03-tree-read/js/GTree.js
+++ 03-tree-read/js/GTree.js
@@ -6,7 +6,11 @@ class GTree {
     static m_instance = null;
     //===============================================
     constructor() {
-
+        this.m_index        = 0;
+        this.m_parentIndex  = 0;
+        this.m_name         = "";
+        this.m_type         = "";
+        this.m_map          = [];
     }
     //===============================================
     static Instance() {
@@ -42,6 +46,64 @@ class GTree {
         var lLis = document.querySelectorAll(".tree1");
         for(var i = 0; i < lLis.length; i++) {
             this.updateArrows(lLis[i]);
+        }
+    }
+    //===============================================
+    serialize() {
+        return this.serializeJson();
+    }
+    //===============================================
+    serializeJson() {
+        var lDom = {};
+
+        lDom["index"]                   = this.m_index;
+        lDom["parent_index"]            = this.m_parentIndex;
+        lDom["name"]                    = this.m_name;
+        lDom["type"]                    = this.m_type;
+
+        if(this.m_map.length) {
+            var lMap = [];
+
+            for(var i = 0; i < this.m_map.length; i++) {
+                var lObj            = this.m_map[i];
+                var lDom2           = {};
+
+                lDom2["index"]          = lObj.m_index;
+                lDom2["parent_index"]   = lObj.m_parentIndex;
+                lDom2["name"]           = lObj.m_name;
+                lDom2["type"]           = lObj.m_type;
+
+                lMap.push(lDom2);
+            }
+            lDom["map"] = lMap;
+        }
+        return JSON.stringify(lDom, null, 2);
+    }
+    //===============================================
+    deserialize(_data) {
+        this.deserializeJson(_data);
+    }
+    //===============================================
+    deserializeJson(_data) {
+        var lDom                = JSON.parse(_data);
+
+        this.m_index            = lDom["index"];
+        this.m_parentIndex      = lDom["parent_index"];
+        this.m_name             = lDom["name"];
+        this.m_type             = lDom["type"];
+
+        var lMap                = lDom["map"] || [];
+
+        for(var i = 0; i < lMap.length; i++) {
+            var lDom2           = lMap[i];
+            var lObj            = new GTree();
+
+            lObj.m_index        = lDom2["index"];
+            lObj.m_parentIndex  = lDom2["parent_index"];
+            lObj.m_name         = lDom2["name"];
+            lObj.m_type         = lDom2["type"];
+
+            this.m_map.push     (lObj);
         }
     }
     //===============================================
@@ -124,27 +186,64 @@ class GTree {
             var lLi = lLis[i];
             if(lLi.classList.contains("tree3")) {
                 console.log(sprintf("%s", lLi.dataset.value));
-                this.readItem(lLi.nextElementSibling, "    ");
+                var lObj            = new GTree();
+                lObj.setIndex       (this.size() + 1);
+                lObj.setParentIndex (this.getIndex());
+                lObj.setName        (lLi.dataset.value);
+                lObj.setType        ("dir");
+                this.add            (lObj);
+                this.readItem(lObj, lLi.nextElementSibling, "    ");
             }
             else {
                 console.log(sprintf("%s", lLi.dataset.value));
+                var lObj            = new GTree();
+                lObj.setIndex       (this.size() + 1);
+                lObj.setParentIndex (this.getIndex());
+                lObj.setName        (lLi.dataset.value);
+                lObj.setType        ("file");
+                this.add            (lObj);
             }
         }
     }
     //===============================================
-    readItem(_parent, _shift) {
+    readItem(_obj, _parent, _shift) {
         var lLis = _parent.querySelectorAll(":scope > li > .tree3, :scope > .tree4");
         for(var i = 0; i < lLis.length; i++) {
             var lLi = lLis[i];
             if(lLi.classList.contains("tree3")) {
                 console.log(sprintf("%s%s", _shift, lLi.dataset.value));
-                this.readItem(lLi.nextElementSibling, _shift + "    ");
+                var lObj            = new GTree();
+                lObj.setIndex       (this.size() + 1);
+                lObj.setParentIndex (_obj.getIndex());
+                lObj.setName        (lLi.dataset.value);
+                lObj.setType        ("dir");
+                this.add            (lObj);
+                this.readItem(lObj, lLi.nextElementSibling, _shift + "    ");
             }
             else {
                 console.log(sprintf("%s%s", _shift, lLi.dataset.value));
+                var lObj            = new GTree();
+                lObj.setIndex       (this.size() + 1);
+                lObj.setParentIndex (_obj.getIndex());
+                lObj.setName        (lLi.dataset.value);
+                lObj.setType        ("file");
+                this.add            (lObj);
             }
         }
     }
+    //===============================================
+    setIndex(_index)                            {this.m_index = _index;}
+    setParentIndex(_parentIndex)                {this.m_parentIndex = _parentIndex;}
+    setName(_name)                              {this.m_name = _name;}
+    setType(_type)                              {this.m_type = _type;}
+    add(_obj)                                   {this.m_map.push(_obj);}
+    //
+    getIndex()                                  {return this.m_index;}
+    getParentIndex()                            {return this.m_parentIndex;}
+    getName()                                   {return this.m_name;}
+    getType()                                   {return this.m_type;}
+    at(i)                                       {return this.m_map[i];}
+    size()                                      {return this.m_map.length;}
     //===============================================
 }
 //===============================================
--- 03-tree-read/js/scripts.js
+++ 03-tree-read/js/scripts.js
@@ -2,5 +2,6 @@
 function call_server() {
     var lTree = new GTree();
     lTree.readTree("#tree");
+    console.log(sprintf("serialisation :\n%s", lTree.serialize()));
 }
 //===============================================
